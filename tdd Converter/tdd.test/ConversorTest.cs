fa5aec78860031d2883a82b26139a33859a1e05f
Karol <carol_andrea@live.com.mx>
2021-10-17 21:51:12 -0500

    Conversor decimal, binario y hexadecimal

---
+++ tdd Converter/tdd.test/ConversorTest.cs
@@ -0,0 +1,115 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tdd.test
+{
+    [TestClass]
+    public class ConversorTest
+    {
+        [TestMethod]
+        public void ConvertDecimalToBinaryTest()
+        {
+            //Arrange
+            var bInit = "10";
+            var bConvert = "2";
+            var value = "8";
+            var convert = new tdd.Conversor();
+            var expectedResult = "1000";
+
+            //Act
+            var result = convert.Conver(bInit, bConvert, value);
+
+            //Assert
+            Assert.AreEqual(result, expectedResult);
+        }
+
+        [TestMethod]
+        public void ConvertHexToBinaryTest()
+        {
+            //Arrange
+            var bInit = "16";
+            var bConvert = "2";
+            var value = "0xa";
+            var convert = new tdd.Conversor();
+            var expectedResult = "1010";
+
+            //Act
+            var result = convert.Conver(bInit, bConvert, value);
+
+            //Assert
+            Assert.AreEqual(result, expectedResult);
+        }
+
+        [TestMethod]
+        public void ConvertBinaryToDecimalTest()
+        {
+            //Arrange
+            var bInit = "2";
+            var bConvert = "10";
+            var value = "1010";
+            var convert = new tdd.Conversor();
+            var expectedResult = "10";
+
+            //Act
+            var result = convert.Conver(bInit, bConvert, value);
+
+            //Assert
+            Assert.AreEqual(result, expectedResult);
+        }
+
+        [TestMethod]
+        public void ConvertHexToDecimalTest()
+        {
+            //Arrange
+            var bInit = "16";
+            var bConvert = "10";
+            var value = "0xa";
+            var convert = new tdd.Conversor();
+            var expectedResult = "10";
+
+            //Act
+            var result = convert.Conver(bInit, bConvert, value);
+
+            //Assert
+            Assert.AreEqual(result, expectedResult);
+        }
+
+        [TestMethod]
+        public void ConvertDecimalToHexTest()
+        {
+            //Arrange
+            var bInit = "10";
+            var bConvert = "16";
+            var value = "10";
+            var convert = new tdd.Conversor();
+            var expectedResult = "a";
+
+            //Act
+            var result = convert.Conver(bInit, bConvert, value);
+
+            //Assert
+            Assert.AreEqual(result, expectedResult);
+        }
+
+        [TestMethod]
+        public void ConvertBinaryToHexTest()
+        {
+            //Arrange
+            var bInit = "2";
+            var bConvert = "16";
+            var value = "1010";
+            var convert = new tdd.Conversor();
+            var expectedResult = "a";
+
+            //Act
+            var result = convert.Conver(bInit, bConvert, value);
+
+            //Assert
+            Assert.AreEqual(result, expectedResult);
+        }
+    }
+}
---
+++ tdd Converter/tdd/Conversor.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Linq;
+
+namespace tdd
+{
+    public class Conversor
+    {
+        public string Conver(string bInit, string bConvert, string value)
+        {
+            switch (bConvert)
+            {
+                case "2":
+                    if(bInit == "10")
+                        return ConvertDecimalToBinary(value);
+                    if (bInit == "16")
+                        return ConvertHexToBinary(value);
+                    break;
+                case "10":
+                    if (bInit == "2")
+                        return ConvertBinaryToDecimal(value);
+                    if (bInit == "16")
+                        return ConvertHexToDecimal(value);
+                    break;
+                case "16":
+                    if (bInit == "10")
+                        return ConvertDecimalToHex(value);
+                    if (bInit == "2")
+                        return ConvertBinaryToHex(value);
+                    break;
+            }
+            return String.Empty;
+        }
+
+        private string ConvertDecimalToBinary(string value)
+        { 
+            // se convierte valor a entero
+            var castedValue = System.Convert.ToInt32(value);
+            var bynaryResult = System.Convert.ToString(castedValue, 2);
+            var result = "";
+
+            while(castedValue > 0)
+            {
+                result += (castedValue % 2).ToString();
+                castedValue = castedValue / 2;
+            }
+            var charResult = result.ToCharArray();
+            Array.Reverse(charResult);
+            //return new String(charResult); 
+            return bynaryResult;
+        }
+
+        private string ConvertHexToBinary(string value)
+        {
+            // se debe mandar el numero en hexa
+            var bin = Convert.ToString(Convert.ToSByte(value, 16), 2);
+            return bin;
+        }
+
+        private string ConvertBinaryToDecimal(string value)
+        {
+            var dec = Convert.ToInt32(value, 2);
+            return dec.ToString();
+        }
+
+        private string ConvertHexToDecimal(string value)
+        {
+            var dec = Convert.ToString(Convert.ToSByte(value, 16), 10);
+            return dec;
+        }
+
+        private string ConvertDecimalToHex(string value)
+        {
+            var castedValue = System.Convert.ToInt32(value);
+            var hex = Convert.ToString(castedValue, 16);
+            return hex;
+        }
+
+        private string ConvertBinaryToHex(string value)
+        {
+            var hex = Convert.ToString(Convert.ToSByte(value, 2), 16);
+            return hex;
+        }
+    }
+}
